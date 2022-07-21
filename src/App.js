ecc83298def3ee29362dd19e82ea8fd105b5ef67
Ain Chung <artnouveau0804@gmail.com>
2022-07-21 15:05:15 +0900

    파일 이동
    이전 프로젝트 파일들 이동.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React from 'react';
-import Today from './components/Today';
-import SetDday from './components/SetDday';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import styled from 'styled-components';
+import Home from './components/Home';
+import Music4u from './components/Music4u';
+import MyMusic from './components/MyMusic';
+import Recommand from './components/Recommand';
+import Search from './components/Search';
+import Lists from './components/Lists';
+import TFS from './components/TFS';
+// import Main from './components/Main';
 
 export default function App() {
   return (
-    <>
-      <Today />
-      <SetDday />
-    </>
+    <Container>
+      <BrowserRouter>
+        <Routes>
+          <Route path={'/lists'} element={<Lists />} />
+          <Route path={'/'} element={<Home />} />
+          {/* <Route path={'/'} element={<Main />}> */}
+          <Route path={'/mymusic'} element={<MyMusic />} />
+          <Route path={'/search'} element={<Search />} />
+          <Route path={'/:id'} element={<Recommand />} />
+        </Routes>
+      </BrowserRouter>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  margin: 10px;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+    Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+`;
